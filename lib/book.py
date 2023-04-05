92a05c5f8223ffed2642364bcecc1af532cf1c53
Lee Campbell <121746971+leevcampbell@users.noreply.github.com>
2023-04-05 16:38:30 -0400

    added functions to book.py and shoes.py

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count=0):
+        self.title = title
+        self.page_count = page_count
+
+    def get_page_count(self):
+        return self._page_count
+    
+    def set_page_count(self, page_count):
+        if not isinstance(page_count, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = page_count
+
+    page_count = property(get_page_count, set_page_count)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+        
+
+        
+# the_shadow_of_the_wind = Book()
+       
+        
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size=0, condition="used"):
+        self.brand = brand
+        self.size = size
+        self.condition = condition
+    
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, size):
+        if not isinstance(size, int):
+            print("Size must be an integer")
+        else:
+            self._size = size
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+        
+
+    size = property(get_size, set_size)
